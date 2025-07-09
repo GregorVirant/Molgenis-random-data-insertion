7b0553bf285207842fa1d6cd3bf585e9aa9e1b66
Gregor Virant <110791157+GregorVirant@users.noreply.github.com>
2025-07-09 15:58:42 +0200

    Update main reading

--- main_reading.py
+++ main_reading.py
@@ -2,36 +2,43 @@ import os
 import dotenv
 import requests
 
-def main():
-    dotenv.load_dotenv()
-    token = os.environ.get("API_KEY")
-    server_url = os.environ.get("SERVER_URL")
-    schema = os.environ.get("SCHEMA")
+dotenv.load_dotenv()
+token = os.environ.get("API_KEY")
+server_url = os.environ.get("SERVER_URL")
+schema = os.environ.get("SCHEMA")
 
-    url = server_url+"/"+schema + "/tables/graphql"
-    headers = {
-        "Content-Type": "application/json",
-        "x-molgenis-token": token
-    }
-    #querying everything connected to a client
+url = server_url+"/"+schema + "/tables/graphql"
+headers = {
+    "Content-Type": "application/json",
+    "x-molgenis-token": token
+}
+
+def main():
     query = """
     {
-        Ticket_kind { 
-            name
-            valid_for_h
-            tickets_usages{
-            client { 
-                    name
-                    surname
-                }
+    Client_table { 
+        name
+        surname
+        date_of_birth
+        created_on
+            country{name}
+            activated_tickets  {
+            price
+            activation_time
+            ticket {
+                name
+                valid_for_h
+                price
             }
         }
+    }
     }"""
+    send_http_request(query)
 
+def send_http_request(query):
     body = {
         "query": query
     }
-
     response = requests.post(url, headers=headers, json=body)
     print(response.status_code)
     print(response.text)
